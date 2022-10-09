d5d712b12a880aae5a0dc5273698c458a7ee5601
harish kumar <harishkunar987@gmail.com>
2022-10-09 22:48:54 +0530

    upgrade

--- MileStone4/MileStone4Lecture33/Map.java
+++ MileStone4/MileStone4Lecture33/Map.java
@@ -15,11 +15,25 @@ public class Map<K,V> {
 
         buckets = new ArrayList<>(); // here we create buckets with ArrayList:
         numBuckets = 20; // here we create the (20) number of    buckets:
-        // here we use forloop for putting (null) values  in that 20  numBuckets:
+        // here we use forloop for putting (null) values  in that 20  numB uckets:
         for(int i = 0; i < numBuckets;i++){
             buckets.add(null);
         }
 
     }
+    // here we create constructor:
+    // from were we are getting our bucketIndex:
+    // bucketIndex is that were we put our key/value pairs:
+    private int getBucketIndex(K key){
+       int  hc = key.hashCode(); // here we use superclass function for getting the hashcode for our ( owm map function) that we are creating:
+       // after getting the hashcode: we have to compress it:
+       // for getting our array index value:
+       int index = hc%numBuckets; // here we get our index using (modulo) function:
+       return index; // and here we are returning our index:
+    }
+
+
+
+
     
 }
