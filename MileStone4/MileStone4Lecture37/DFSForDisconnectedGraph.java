f74801b4eae0b520ea609aa30c819d8f81013d2d
harish kumar <harishkunar987@gmail.com>
2023-01-01 10:27:10 +0530

    upgrade

---
+++ MileStone4/MileStone4Lecture37/DFSForDisconnectedGraph.java
@@ -0,0 +1,7 @@
+package MileStone4.MileStone4Lecture37;
+
+public class DFSForDisconnectedGraph {
+  // here we are writing a code for the (disconnect-graph).means If In graph we have two components: 
+  // then what will the (DFS) code for that: or we can say that what are the changes that we have to make in our simple (DFS) code.so that it will also work for the more than one component graph:
+
+}
